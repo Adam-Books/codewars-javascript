8ec7c96f016f8f547255d05c1c9f0a0527d9e65d
Adam-Books <adam.arabicbooks@gmail.com>
2021-11-26 21:17:35 +0200

    Codewars Katas - JavaScript solutions

---
+++ 8-kyu/!a == a !.js
@@ -0,0 +1,25 @@
+/*
+  Title:
+    !a == a ?!
+  
+  Description:
+    Your task is to pass only these tests:
+        a == false
+        !a == false
+        a == !a
+  
+  Kata Link:
+    https://www.codewars.com/kata/59f9796cffe75f9299000025
+  
+*/
+
+// Solutions:
+
+// Solution 1:
+const a = "0";
+
+// Solution 2:
+const a = [];
+
+// Solution 3:
+const a = " ";
