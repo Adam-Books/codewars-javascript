81d78983b21488b8a6e25eb1ca0d5696f81bf5e4
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2021-11-30 07:17:22 +0200

    Create 5 without numbers !!.js

---
+++ 8-kyu/5 without numbers !!.js
@@ -0,0 +1,25 @@
+/*
+  Title:
+    5 without numbers !!
+  
+  Description:
+    Write a function that always returns 5
+
+    Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+    Good luck :)
+  
+  Kata Link:
+    https://www.codewars.com/kata/59441520102eaa25260000bf
+  
+*/
+
+// Solutions:
+
+// Solution 1:
+unusualFive=_=>'     '.length
+
+// Solution 2:
+
+
+// Solution 3:
