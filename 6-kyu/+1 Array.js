19e5eae8edbd07913530800c4f4bca0264e35538
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2021-12-24 09:36:49 +0200

    +1 Array - JavaScript Solution

--- 6-kyu/+1 Array.js
+++ 6-kyu/+1 Array.js
@@ -21,5 +21,20 @@ Link:
 
 // Solutions:
 // Solution 1:
+const upArray = (arr) => {
+  if (arr.length === 0) return null;
+  let carry = 1;
+  for (let i = arr.length-1; i >= 0; i--) {
+    if (arr[i] > 9 || arr[i] < 0) return null;
+    if (carry > 0) {
+      arr[i] += carry;
+      carry = Math.floor(arr[i] / 10);
+      arr[i] = arr[i] % 10;
+    }
+  }
+  if (carry > 0) arr.unshift(carry);
+  return arr;
+}
+
 
 
