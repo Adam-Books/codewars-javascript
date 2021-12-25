2bd0e950c2006606f62d59a5e3fceb400e6f0148
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2021-12-25 16:05:07 +0200

    Multiply - JavaScript Solution

---
+++ 8-kyu/Multiply.js
@@ -0,0 +1,21 @@
+/*
+Title:
+  Multiply
+  
+Description:
+  This code does not execute properly. Try to figure out why.  
+  
+Link:
+  https://www.codewars.com/kata/50654ddff44f800200000004
+
+*/
+
+// Solutions:
+// Solution 1:
+function multiply(a, b) {
+ return a * b
+}
+
+// Solution 2:
+const multiply = (a, b) => a * b;
+
