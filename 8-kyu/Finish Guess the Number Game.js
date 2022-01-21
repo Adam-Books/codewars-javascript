1438d14b1b49f3f500086ea5b66367d2586bdb6c
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-01-21 16:49:43 +0200

    Update Finish Guess the Number Game.js

--- 8-kyu/Finish Guess the Number Game.js
+++ 8-kyu/Finish Guess the Number Game.js
@@ -4,7 +4,8 @@ Title
   Finish Guess the Number Game
   
 Description:
-  Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+  Imagine you are creating a game where the user has to guess the correct number. 
+  But there is a limit of how many guesses the user can do.
 
     If the user tries to guess more than the limit, the function should throw an error.
     If the user guess is right it should return true.
@@ -19,6 +20,21 @@ Link:
 
 // Solutions:
 // Solution 1:
+class Guesser {
+  constructor(number, lives) {
+    this.number = number;
+    this.lives = lives;
+  }
+  
+  guess(n) {
+    if (!this.lives) throw new Error('You have no more life')
+    if (n === this.number) return true
+
+    this.lives--
+    return false
+  }
+}
+
 
 
 
