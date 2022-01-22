534f40f7e320418b8fa38184e4cb236a5aa16037
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-01-22 09:06:47 +0200

    Create Basic variable assignment.js

---
+++ 8-kyu/Basic variable assignment.js
@@ -0,0 +1,18 @@
+/*
+
+Title:
+  Basic variable assignment
+  
+Description:
+  This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+Link:
+  https://www.codewars.com/kata/50ee6b0bdeab583673000025
+  
+*/
+
+// Solutions:
+// Solution 1:
+
+
+
