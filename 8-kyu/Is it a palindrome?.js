12b3e4d96b394ef732992fd981ff3f4321a1fcc0
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-01-24 07:36:39 +0200

    Create Is it a palindrome?.js

---
+++ 8-kyu/Is it a palindrome?.js
@@ -0,0 +1,18 @@
+/*
+
+Title:
+  Is it a palindrome?
+  
+Description:
+  Write a function that checks if a given string (case insensitive) is a palindrome.
+  
+Link:
+  https://www.codewars.com/kata/57a1fd2ce298a731b20006a4
+  
+*/
+
+// Solutions:
+// Solution 1:
+
+
+
